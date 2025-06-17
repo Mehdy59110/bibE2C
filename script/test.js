e739384f8b1dbe55f99cff64b8310b7e26be6ad7
Mehdy59110 <mehdy.lenne59110@gmail.com>
2025-06-17 11:46:14 +0200

    script suite

--- script/test.js
+++ script/test.js
@@ -1,3 +1,45 @@
+let trigger = document.querySelector('#send-button');
+let numberzone = document.querySelector('#user-number');
+let displayzone = document.querySelector('#game-display');
+
+let targetNumber = Math.floor(Math.random ()*100)+1;
+
+console.log( trigger, numberzone, displayzone, targetNumber);
+
+function compareNumber() {
+    //console.log("ça marche");
+    let userNumber = numberzone.value;
+    let result;
+    //console.log(userNumber);
+    if(userNumber==targetNumber){
+        result='bravo';
+    }else if(userNumber > targetNumber) {
+        result=`C'est trop grand!`;
+    } else {
+        result=`C'est trop petit!`;
+    }
+
+    let resultZone = document.createElement('p');
+    resultZone.innerHTML = result;
+    resultZone.style.padding = "10px";
+    resultZone.style.backgroundColor = "var(--couleur-midfonce)";
+    resultZone.classList.add('result-line');
+
+    //console.log(resultZone);
+    displayzone.appendChild(resultZone);
+    
+}
+    
+    
+
+
+
+trigger.addEventListener('click', compareNumber);
+
+
+
+
+/*
 let targetNumber = math.floor(math . random ()*100)+1;
 console.log( targetNumber);
 let userNumber = prompt('Devinez un nombre entre 1 et 100 :');
@@ -14,15 +56,4 @@ while(userNumber != targetNumber) {
 }
 
 alert('Bravo, vous n\'avez trouvé!');
-
-
-
-
-
-
-/*
-let firstname = prompt('Quel est votre prénom?');
-let lastname = prompt("Quel est votre nom?");
-
-alert(`Bonjour ${firstname} ${lastname}!`);
 */
